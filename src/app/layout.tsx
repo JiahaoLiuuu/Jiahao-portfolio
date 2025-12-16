f9f9b5858f5ac52328618c4707593f2b89e1c208
JiahaoLiuuu <130607899+JiahaoLiuuu@users.noreply.github.com>
2025-12-15 23:13:51 -0600

    Change font styles
    Updated font imports and adjusted metadata for the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Press_Start_2P, VT323 } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../components/component/theme-provider"
 import { Analytics } from '@vercel/analytics/react';
 
+const pressStart2P = Press_Start_2P({ 
+  weight: "400", 
+  subsets: ["latin"],
+  variable: "--font-pixel-header" 
+});
 
-const inter = Inter({ subsets: ["latin"] });
+const vt323 = VT323({ 
+  weight: "400", 
+  subsets: ["latin"],
+  variable: "--font-pixel-body" 
+});
 
 export const metadata: Metadata = {
-
-  title: "Welcome to Jiahao's Portfolio Site",
-  description: "Hi there!",
+  title: "Jiahao's Pixel Portfolio",
+  description: "Level 1 Software Developer",
 };
 
 export default function RootLayout({
@@ -20,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-              <body>
+      <body className={`${pressStart2P.variable} ${vt323.variable} font-pixel-body`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
